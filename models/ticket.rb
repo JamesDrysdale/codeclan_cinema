15fc4cd3fc586bee297ae929ad1755e933f5a2ad
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-04-17 13:25:24 +0100

    Initialize Ticket class

---
+++ models/ticket.rb
@@ -0,0 +1,14 @@
+require_relative("../db/sql_runner")
+
+class Ticket
+    attr_reader :id
+    attr_accessor :customer_id, :film_id
+
+    def initialize
+        @id = options['id'].to_i if options['id']
+        @customer_id = options['customer_id']
+        @film_id = options['film_id'].to_i
+    end
+
+
+end 
